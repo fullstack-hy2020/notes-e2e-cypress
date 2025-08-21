3ccd50366bdac75cc751af4120c68f7e5efd6363
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-08-21 13:58:01 +0300

    Add changes from yearly check 2025

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,5 +1,68 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Note app', function () {
+  beforeEach(function () {
+    cy.request('POST', '/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', '/api/users/', user)
+    cy.visit('')
+  })
+
+  it('front page can be opened', function () {
+    cy.contains('Notes')
+    cy.contains(
+      'Note app, Department of Computer Science, University of Helsinki 2025'
+    )
+  })
+
+  it('user can login', function () {
+    cy.contains('button', 'login').click()
+    cy.contains('label', 'username').type('mluukkai')
+    cy.contains('label', 'password').type('salainen')
+    cy.get('#login-button').click()
+
+    cy.contains('Matti Luukkainen logged in')
+  })
+
+  it('login fails with wrong password', function () {
+    cy.contains('button', 'login').click()
+    cy.contains('label', 'username').type('mluukkai')
+    cy.contains('label', 'password').type('wrong')
+    cy.get('#login-button').click()
+
+    cy.get('.error')
+      .should('contain', 'wrong credentials')
+      .and('have.css', 'color', 'rgb(255, 0, 0)')
+      .and('have.css', 'border-style', 'solid')
+
+    cy.contains('Matti Luukkainen logged in').should('not.exist')
+  })
+
+  describe('when logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'mluukkai', password: 'salainen' })
+    })
+
+    it('a new note can be created', function () {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+    describe('and several notes exist', function () {
+      beforeEach(function () {
+        cy.createNote({ content: 'first note', important: true })
+        cy.createNote({ content: 'second note', important: true })
+        cy.createNote({ content: 'third note', important: true })
+      })
+
+      it('one of those can be made non important', function () {
+        cy.contains('second note').parent().find('button').as('theButton')
+        cy.get('@theButton').click()
+        cy.get('@theButton').should('contain', 'make important')
+      })
+    })
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,33 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
 Cypress.Commands.add('login', ({ username, password }) => {
-  cy.request('POST',  `${Cypress.env('BACKEND')}/login`, {
-    username, password
+  cy.request('POST', '/api/login', {
+    username,
+    password
   }).then(({ body }) => {
     localStorage.setItem('loggedNoteappUser', JSON.stringify(body))
     cy.visit('')
@@ -9,11 +36,13 @@ Cypress.Commands.add('login', ({ username, password }) => {
 
 Cypress.Commands.add('createNote', ({ content, important }) => {
   cy.request({
-    url: `${Cypress.env('BACKEND')}/notes`,
+    url: '/api/notes',
     method: 'POST',
     body: { content, important },
     headers: {
-      'Authorization': `Bearer ${JSON.parse(localStorage.getItem('loggedNoteappUser')).token}`
+      Authorization: `Bearer ${
+        JSON.parse(localStorage.getItem('loggedNoteappUser')).token
+      }`
     }
   })
 
